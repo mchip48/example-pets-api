283cc4ee67eebd48cce1f79f77a5586db94beb81
Matthew Chipkin <mchipkin21@gmail.com>
2025-12-03 20:31:35 -0600

    Uncommented bcrypt in gemfile and generated user model. also did rails db:migrate to update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_04_022202) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_04_022958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_04_022202) do
     t.string "breed"
     t.datetime "created_at", null: false
     t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 end
